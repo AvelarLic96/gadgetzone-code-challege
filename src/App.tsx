c8f8591014a5e2d7e03d08751717e5a729c34a79
Oscar Avelar <oscar.avelar10@gmail.com>
2024-03-05 21:33:31 -0600

    💄 Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 import Home from './pages/Home/Index'
 
 function App() {
   return (
-    <Home />
+    <>
+      <Header />
+      <Home />
+    </>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+const Header = () => {
+  return (
+    <header className="header">
+      <div className='header-container'>
+        <div className='header-title'>GadgetZone</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
